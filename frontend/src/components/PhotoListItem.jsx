b29644e3121d2a3dc496dd469a147af28638da53
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-12 11:16:52 -0400

    Fix PhotoListItem with props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { photo } = props;
+  return (
+    <>
+  <img src={photo.imageSource} alt={`Photo by ${photo.username}`}/>
+  <img src={photo.profile} alt={`${photo.username}'s profile`}/>
+  <p>{photo.username}</p>
+  <p>{photo.location.city}, {photo.location.country}</p>
+  </>
+  );
 };
 
 export default PhotoListItem;
