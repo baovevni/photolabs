8f43a3ba6eebfed9a4edb137e5d39b3cccc8c482
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-12 18:20:48 -0400

    Remove props from PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,7 +9,7 @@ import PhotoFavButton from './PhotoFavButton';
 const PhotoListItem = ({ photo }) => {
   return (
     <div className="photo-list__item">
-      <PhotoFavButton photo={photo}/>
+      <PhotoFavButton />
       <img className="photo-list__image" src={photo.imageSource} alt={`Photo by ${photo.username}`} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.profile} alt={`${photo.username}'s profile`} />
