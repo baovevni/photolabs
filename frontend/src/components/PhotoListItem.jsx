cfcbe310fd25e89cfc4d5bcb231579169982ca4e
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-12 15:09:32 -0400

    add stylings to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,12 +7,16 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const { photo } = props;
   return (
-    <>
-  <img src={photo.imageSource} alt={`Photo by ${photo.username}`}/>
-  <img src={photo.profile} alt={`${photo.username}'s profile`}/>
-  <p>{photo.username}</p>
-  <p>{photo.location.city}, {photo.location.country}</p>
-  </>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={photo.imageSource} alt={`Photo by ${photo.username}`} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photo.profile} alt={`${photo.username}'s profile`} />
+        <div className="photo-list__user-info">
+          <p>{photo.username}</p>
+          <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
