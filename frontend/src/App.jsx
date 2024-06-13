d817924f4d0cb3edb43d97a8d6a9150b3cfcbfbe
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-12 20:05:20 -0400

    Add TopicList feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,17 @@
 import React from 'react';
 
 import PhotoList from './components/PhotoList';
-import './App.scss';
+import TopicList from './components/TopicList';
 
-// const sampleDataForPhotoListItem = [{
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// },
-// {
-//   id: "2",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// },
-// {
-//   id: "3",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// }
-// ]
+import './App.scss';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = sampleDataForPhotoListItem.map((photo) => {
-  //   return <PhotoListItem key={photo.id} photo={photo} />;
-  // });
-  
+
   return (
     <div className="App">
-      {/* {photos} */}
+      <TopicList />
       <PhotoList />
     </div>
   );
