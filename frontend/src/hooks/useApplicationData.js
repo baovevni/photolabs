90dd03c2ac03a2205ccf1688a0eef12989e72c05
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-15 13:21:31 -0400

    Add useReducer instead of useState

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,38 +1,61 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
-const useFavorite = () => {
-  const [favoritePhotos, setFavoritePhotos] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
+const initialState = {
+  favoritePhotos: [],
+  isModalOpen: false,
+  selectedPhoto: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_FAVORITE':
+      return {
+        ...state,
+        favoritePhotos: state.favoritePhotos.includes(action.photoId)
+          ? state.favoritePhotos.filter((id) => id !== action.photoId)
+          : [...state.favoritePhotos, action.photoId],
+      };
+    case 'SET_IS_MODAL_OPEN':
+      return {
+        ...state,
+        isModalOpen: action.isModalOpen,
+      };
+    case 'SET_SELECTED_PHOTO':
+      return {
+        ...state,
+        selectedPhoto: action.photo,
+      };
+    default:
+      return state;
+  }
+};
+
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavorite = (photoId) => {
-    if (favoritePhotos.includes(photoId)) {
-      setFavoritePhotos(favoritePhotos.filter((id) => id !== photoId));
-    } else {
-      setFavoritePhotos([...favoritePhotos, photoId]);
-    }
+    dispatch({ type: 'TOGGLE_FAVORITE', photoId });
   };
 
   const handleImageClick = (photo) => {
-    setSelectedPhoto(photo);
-    setIsModalOpen(true);
+    dispatch({ type: 'SET_SELECTED_PHOTO', photo });
+    dispatch({ type: 'SET_IS_MODAL_OPEN', isModalOpen: true });
   };
 
   const isFavorite = (photo) => {
-    return (
-      favoritePhotos?.length !== 0 ? favoritePhotos.includes(`${photo.id}`) : false)
-  }
+    return state.favoritePhotos.includes(photo.id);
+  };
 
   return {
-    favoritePhotos,
-    isModalOpen,
-    selectedPhoto,
+    favoritePhotos: state.favoritePhotos,
+    isModalOpen: state.isModalOpen,
+    selectedPhoto: state.selectedPhoto,
     toggleFavorite,
     handleImageClick,
-    setIsModalOpen,
-    setSelectedPhoto,
-    isFavorite
+    setIsModalOpen: (isModalOpen) => dispatch({ type: 'SET_IS_MODAL_OPEN', isModalOpen }),
+    setSelectedPhoto: (photo) => dispatch({ type: 'SET_SELECTED_PHOTO', photo }),
+    isFavorite,
   };
 };
 
-export default useFavorite;
+export default useApplicationData;
